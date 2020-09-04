036554bf171a80e8c9f42c609e0fc378a3bdd1be
Chris Bryant <QuantumSinger@gmail.com>
2020-09-04 18:00:23 -0500

    latest changes

---
+++ src/L19/Chris/getLogThreshLim.m
@@ -0,0 +1,22 @@
+function logThresh = getLogThreshLim(logZone,varargin)
+
+    if nargin>1
+        THRESH_LIM = varargin{1};
+    else
+        THRESH_LIM = 1.6e-7; % ergs / cm^2 
+        %THRESH_LIM = 5.e-6; % ergs / cm^2 
+    end
+
+    MPC2CM = 3.09e24;   % 1 Mega Parsec = MPC2CM centimeters.
+    LOGMPC2CMSQ4PI = log(4.0*pi) + 2.0*log(MPC2CM);     % log(MegaParsec2centimeters).
+    LOG_THRESH_LIM = log(THRESH_LIM);% + 3;
+    zone = exp(logZone);
+    logThresh = LOGMPC2CMSQ4PI ...
+              + 2*getLogLumDisWicMPC(zone) ...
+              + LOG_THRESH_LIM ...
+              ... - 1.197344248356904e+02 ... 52*log(10)
+              - logZone ... % comment out this line for detector threshold based on Liso/flux
+              + log(getZoneCorrection(zone)) ...
+              ;
+
+end
--- src/L19/reproduceL19Liso.m
+++ src/L19/reproduceL19Liso.m
@@ -12,14 +12,15 @@ addpath(genpath("../"),"-begin");
 fontSize = 13;
 lineWidth = 1.5;
 figureColor = "white";
-freshRunEnabled = false; % this must be set to true for first ever simulation. Thereafter, it can be set to false to save time.
+freshRunEnabled = true; % this must be set to true for first ever simulation. Thereafter, it can be set to false to save time.
 
 if freshRunEnabled
 
     L19 = struct();
-    L19.thresh.val = 1.6e-07; % to match threshold line
+    %L19.thresh.val = 1.6e-07; % to match threshold line
     %L19.thresh.val = 2e-06; % value in paper
     %L19.thresh.val = 7e-10; % to get alpha = 2.30
+    L19.thresh.val = 7.e-7;
     L19.thresh.logVal = log(L19.thresh.val);
 
     L19.input.file.path = "../../in/L19figure1.xlsx";
@@ -27,14 +28,14 @@ if freshRunEnabled
     L19.input.file.contents.sorted = sortrows(L19.input.file.contents.data,1);
     L19.output.path = "../../out/L19"; if ~isfolder(L19.output.path); mkdir(L19.output.path); end
     L19.zone = L19.input.file.contents.sorted(:,1) + 1;
-    L19.eiso = L19.input.file.contents.sorted(:,3)*1.e52;
+    L19.liso = 1.e52*L19.input.file.contents.sorted(:,3).*L19.zone./L19.input.file.contents.sorted(:,2);
     L19.logZone = log(L19.zone);
-    L19.logEiso = log(L19.eiso);
+    L19.logLiso = log(L19.liso);
 
     L19.estat = EfronStat   ( L19.logZone ... logx
-                            , L19.logEiso ... logy
+                            , L19.logLiso ... logy
                             , L19.thresh.logVal ... observerLogThresh
-                            , "fluence" ... threshType
+                            , "flux" ... threshType
                             );
 
     L19.logPbol = L19.estat.logyDistanceFromLogThresh + L19.thresh.logVal;
@@ -49,17 +50,17 @@ if freshRunEnabled
     L19.thresh.logZoneLimits = [1,12];
     L19.thresh.logZone = log(L19.thresh.logZoneLimits(1)):0.02:log(L19.thresh.logZoneLimits(2)); % the range of z+1 for which the detection threshold will be drawn.
     L19.thresh.zone = exp(L19.thresh.logZone);
-    L19.thresh.logEiso = L19.thresh.logVal + getLogEisoLumDisTerm(L19.thresh.zone);
-    L19.thresh.eiso = exp(L19.thresh.logEiso);
+    L19.thresh.logLiso = L19.thresh.logVal + getLogLisoLumDisTerm(L19.thresh.zone);
+    L19.thresh.liso = exp(L19.thresh.logLiso);
     figure("color", figureColor); hold on; box on;
     plot( L19.zone ...
-        , L19.eiso ...
+        , L19.liso ...
         , "." ...
         , "markersize", 15 ...
         , "linewidth", lineWidth ...
         );
     plot( L19.thresh.zone ...
-        , L19.thresh.eiso ...
+        , L19.thresh.liso ...
         , "color", "black" ...
         , "linewidth", lineWidth ...
         );
@@ -69,18 +70,18 @@ if freshRunEnabled
     %}
     for i = 1:L19.thresh.logRangeLen
         L19.estatList{i} = EfronStat( L19.logZone ... logx
-                                    , L19.logEiso ... logy
+                                    , L19.logLiso ... logy
                                     , L19.thresh.logRange(i) ... observerLogThresh
-                                    , "fluence" ... threshType
+                                    , "flux" ... threshType
                                     );
     end
 
-    save(L19.output.path + "/L19.mat","L19");
+    save(L19.output.path + "/L19liso_7e-7.mat","L19");
 
 else
     
     L19.output.path = "../../out/L19";
-    load(L19.output.path + "/L19.mat"); % loads L19 object
+    load(L19.output.path + "/L19liso.mat"); % loads L19 object
     
 end
 
@@ -105,12 +106,12 @@ figure("color", figureColor); hold on; box on;
         );
     xline(L19.thresh.val,"linewidth", 2, "linestyle", "--", "color", [0,0,0,0.3]);
     scatter(L19.thresh.val,L19.estat.logxMax.tau,100,'black')
-    annotation('textarrow',[.48,.53],[.75,.75],'String','L19 detection threshold','fontsize',11);
-    annotation('textarrow',[.48,.53],[.25,.25],'String','\tau = -6.15','fontsize',11);
+    %annotation('textarrow',[.48,.53],[.75,.75],'String','L19 detection threshold','fontsize',11);
+    %annotation('textarrow',[.48,.53],[.25,.25],'String','\tau = -6.15','fontsize',11);
     xlabel("Detection Threshold Flux [ergs / s / cm^2]", "interpreter", "tex", "fontsize", fontSize);
     ylabel("Efron-Petrosian Tau Statistic \tau at \alpha = 0", "interpreter", "tex", "fontsize", fontSize);
     set(gca, 'xscale', 'log', 'yscale', 'linear', "color", figureColor);
-    export_fig(L19.output.path + "/L19threshTau.png", "-m4 -transparent")
+    export_fig(L19.output.path + "/L19threshTauLiso.png", "-m4 -transparent")
 hold off;
 
 % plot alpha (tau = 0) versus threshold
@@ -124,63 +125,62 @@ figure("color", figureColor); hold on; box on;
     xline(L19.thresh.val,"linewidth", 2, "linestyle", "--", "color", [0,0,0,0.3]);
     %yline(0,"linewidth", 2, "linestyle", "--", "color", [1,0,1]);
     scatter(L19.thresh.val, L19.estat.logxMax.alpha.tau.zero, 100, 'black');
-    %scatter(4.5e-7, 0, 100, [1,0,1]);
-    annotation('textarrow',[.48,.53],[.4,.4],'String','L19 detection threshold','fontsize',11);
-    annotation('textarrow',[.48,.53],[.72,.72],'String','\alpha = 1.34','fontsize',11);
+    %annotation('textarrow',[.48,.53],[.4,.4],'String','L19 detection threshold','fontsize',11);
+    %annotation('textarrow',[.48,.53],[.72,.72],'String','\alpha = 1.34','fontsize',11);
     %annotation('textarrow',[.7,.65],[.515,.465],'String','flux = 2.214e-7','fontsize',11);
     xlabel("Detection Threshold Flux [ergs / s / cm^2]", "interpreter", "tex", "fontsize", fontSize);
     ylabel("\alpha at Efron-Petrosian Tau Statistic \tau = 0", "interpreter", "tex", "fontsize", fontSize);
     set(gca, 'xscale', 'log', 'yscale', 'linear', "color", figureColor);
-    export_fig(L19.output.path + "/L19threshAlpha.png", "-m4 -transparent")
+    export_fig(L19.output.path + "/L19threshAlphaLiso.png", "-m4 -transparent")
 hold off;
 
-% plot the original bivariate data for zone-eiso
+% plot the original bivariate data for zone-liso
 
 L19.thresh.logZoneLimits = [1,12];
 L19.thresh.logZone = log(L19.thresh.logZoneLimits(1)):0.02:log(L19.thresh.logZoneLimits(2)); % the range of z+1 for which the detection threshold will be drawn.
 L19.thresh.zone = exp(L19.thresh.logZone);
-L19.thresh.logEiso = L19.thresh.logVal + getLogEisoLumDisTerm(L19.thresh.zone);
-L19.thresh.eiso = exp(L19.thresh.logEiso);
+L19.thresh.logLiso = L19.thresh.logVal + getLogLisoLumDisTerm(L19.thresh.zone);
+L19.thresh.liso = exp(L19.thresh.logLiso);
 
 L19.getZcorrection = @(logZone) L19.estat.logxMax.alpha.tau.zero * logZone;
-L19.corrected.logEiso = L19.logEiso(:) - L19.getZcorrection( L19.logZone(:) );
-L19.corrected.eiso = exp(L19.corrected.logEiso);
-L19.corrected.thresh.logEiso = L19.thresh.logEiso(:) - L19.getZcorrection( L19.thresh.logZone(:) );
-L19.corrected.thresh.eiso = exp(L19.corrected.thresh.logEiso);
+L19.corrected.logLiso = L19.logLiso(:) - L19.getZcorrection( L19.logZone(:) );
+L19.corrected.liso = exp(L19.corrected.logLiso);
+L19.corrected.thresh.logLiso = L19.thresh.logLiso(:) - L19.getZcorrection( L19.thresh.logZone(:) );
+L19.corrected.thresh.liso = exp(L19.corrected.thresh.logLiso);
 
 % fit the data by keeping the slope constant
 
 L19.regression.slope  = L19.estat.logxMax.alpha.tau.zero;
-L19.regression.getLogEiso = @(intercept, logZone) intercept + L19.getZcorrection(logZone);
-L19.getSumDistSq = @(intercept) sum( (L19.logEiso - L19.regression.getLogEiso(intercept,L19.logZone)).^2 );
+L19.regression.getLogLiso = @(intercept, logZone) intercept + L19.getZcorrection(logZone);
+L19.getSumDistSq = @(intercept) sum( (L19.logLiso - L19.regression.getLogLiso(intercept,L19.logZone)).^2 );
 L19.regression.intercept = fminsearch( L19.getSumDistSq , 120 );
 L19.regression.logZone = min(L19.logZone):0.05:max(L19.logZone);
-L19.regression.logEiso = L19.regression.getLogEiso(L19.regression.intercept, L19.regression.logZone);
+L19.regression.logLiso = L19.regression.getLogLiso(L19.regression.intercept, L19.regression.logZone);
 
 % fit the detection threshold
 
 L19.thresh.regression.pointIndices = [ length(L19.thresh.logZone) - 5 , length(L19.thresh.logZone) ];
 L19.thresh.regression.logZoneLimits = [ L19.thresh.logZone(L19.thresh.regression.pointIndices(1)) , L19.thresh.logZone(L19.thresh.regression.pointIndices(2)) ];
-L19.thresh.regression.logEisoLimits = [ L19.thresh.logEiso(L19.thresh.regression.pointIndices(1)) , L19.thresh.logEiso(L19.thresh.regression.pointIndices(2)) ];
-L19.thresh.regression.slope = ( L19.thresh.regression.logEisoLimits(2) - L19.thresh.regression.logEisoLimits(1) ) ...
+L19.thresh.regression.logLisoLimits = [ L19.thresh.logLiso(L19.thresh.regression.pointIndices(1)) , L19.thresh.logLiso(L19.thresh.regression.pointIndices(2)) ];
+L19.thresh.regression.slope = ( L19.thresh.regression.logLisoLimits(2) - L19.thresh.regression.logLisoLimits(1) ) ...
                             / ( L19.thresh.regression.logZoneLimits(2) - L19.thresh.regression.logZoneLimits(1) );
 
-% plot zone-eiso
+% plot zone-liso
 
 figure("color", figureColor); hold on; box on;
     plot( L19.zone ...
-        , L19.eiso ...
+        , L19.liso ...
         , "." ...
         , "markersize", 15 ...
         , "linewidth", lineWidth ...
         );
     plot( L19.thresh.zone ...
-        , L19.thresh.eiso ...
+        , L19.thresh.liso ...
         , "color", "black" ...
         , "linewidth", lineWidth ...
         );
     plot( exp( L19.regression.logZone ) ...
-        , exp( L19.regression.logEiso ) ...
+        , exp( L19.regression.logLiso ) ...
         , "--" ...
         , "color", [1,0,1] ...
         , "linewidth", 1.5 * lineWidth ...
@@ -193,35 +193,35 @@ figure("color", figureColor); hold on; box on;
     %yline(4.5e54,"linewidth", 2, "linestyle", "--", "color", [0,0,0,0.3]);
     xlim(L19.thresh.logZoneLimits);
     xlabel("z + 1", "interpreter", "tex", "fontsize", fontSize);
-    ylabel("E_{iso} [ ergs ]", "interpreter", "tex", "fontsize", fontSize);
+    ylabel("L_{iso} [ ergs / s ]", "interpreter", "tex", "fontsize", fontSize);
     legend(["L19 sample", "L19 detection limit","Regression line slope = \alpha"], "interpreter", "tex", "location", "southeast", "fontSize", fontSize,'color',figureColor)
     set(gca, 'xscale', 'log', 'yscale', 'log', "color", figureColor);
-    export_fig(L19.output.path + "/L19zoneEiso.png", "-m4 -transparent")
+    export_fig(L19.output.path + "/L19zoneLiso.png", "-m4 -transparent")
 hold off;
 
 
-% Now build the decorrelated data and plot the redshift-corrected bivariate data for zone-eiso
+% Now build the decorrelated data and plot the redshift-corrected bivariate data for zone-liso
 
 
 figure("color", figureColor); hold on; box on;
     plot( L19.zone ...
-        , L19.corrected.eiso ...
+        , L19.corrected.liso ...
         , "." ...
         , "markersize", 15 ...
         ..., "color", "black" ...
         , "linewidth", lineWidth ...
         );
     plot( L19.thresh.zone ...
-        , L19.corrected.thresh.eiso ...
+        , L19.corrected.thresh.liso ...
         , "color", "black" ...
         , "linewidth", lineWidth ...
         );
     xlim(L19.thresh.logZoneLimits);
     xlabel("z + 1", "interpreter", "tex", "fontsize", fontSize);
-    ylabel("E_{0} [ ergs ]", "interpreter", "tex", "fontsize", fontSize);
+    ylabel("L_{0} [ ergs / s ]", "interpreter", "tex", "fontsize", fontSize);
     legend(["L19 sample", "L19 detection limit"], "interpreter", "tex", "location", "southeast", "fontSize", fontSize,'color',figureColor)
     set(gca, 'xscale', 'log', 'yscale', 'log', "color", figureColor);
-    export_fig(L19.output.path + "/L19zoneEisoCorrected.png", "-m4 -transparent")
+    export_fig(L19.output.path + "/L19zoneLisoCorrected.png", "-m4 -transparent")
 hold off;
 %{
 figure("color", figureColor); hold on; box on;
