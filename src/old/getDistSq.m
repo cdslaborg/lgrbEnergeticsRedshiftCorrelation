f1d191af908052185185ed349c5c61fd8dc6f565
Amir Shahmoradi <a.shahmoradi@gmail.com>
2020-10-12 18:30:45 -0500

    latest build

--- src/old/getDistSq.m
+++
@@ -1,4 +0,0 @@
-function distSq = getDistSq(x)
-    global logZoneCurrent logLisoCurrent
-    distSq = (getLogThreshLim(x) - logLisoCurrent ).^2 + (x-logZoneCurrent).^2;
-end
--- src/old/plotEfronStat.m
+++
@@ -1,7 +0,0 @@
-function plotEfronStat(xvec,yvec,getThreshLim)
-    xmax = getMaxRedshift  ( xvec ... xvec
-                    , yvec ... yvec
-                    , getThreshLim ... getThreshLim
-                    );
-    
-end
